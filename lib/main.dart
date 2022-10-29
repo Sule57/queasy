76e0cd962bc6e9390a787af69b67f3282904cd8d
Savo Simeunovic <62600711+Rek27@users.noreply.github.com>
2022-10-29 12:14:48 +0200

    Fixed problem with Web app (added the parameters)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:queasy/activities/widget_tree.dart';
+
 import 'constants/app_themes.dart';
 import 'constants/navigation.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: const FirebaseOptions(
+    apiKey: 'AIzaSyCNGjdJ0j86h8b_Bk7d9ts-hY4JZ7aNWcQ',
+    appId: 'quizzapp-eb0f2',
+    messagingSenderId: '1:17686953226:web:81a053f17c2b317edd0ef3',
+    projectId: '17686953226',
+  ),);
   runApp(const MyApp());
 }
 
