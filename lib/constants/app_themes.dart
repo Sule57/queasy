6ee78649537105a8532c20f53b43bbb1cc53ba70
parkoriann <debiuti2@gmail.com>
2022-11-02 13:04:16 +0100

    quiz view

--- lib/constants/app_themes.dart
+++ lib/constants/app_themes.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppThemes {
   ThemeData themeData = ThemeData(
     //COLORS
-    primarySwatch: Colors.deepPurple,
     colorScheme: const ColorScheme.light(
-        //TODO fill colors
-        ),
+      primary: Color(0xff72479d),
+      secondary: Color(0xfff19c79),
+      background: Color(0xfff1ffe7),
+      onPrimary: Color(0xfff1ffe7),
+      onSecondary: Color(0xff0d0106),
+      onBackground: Color(0xff0d0106),
+      primaryContainer: Color(0xfff19c79),
+      secondaryContainer: Color(0xff9fc490),
+      //TODO fill necessary colors
+    ),
 
     //TEXT
-    // fontFamily: ,
+    fontFamily: GoogleFonts.nunito().fontFamily,
     textTheme: const TextTheme(
-        //TODO fill text themes
-        ),
+      headline3: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+      ),
+
+      //TODO fill text themes
+    ),
 
     //TEXT FIELD
     inputDecorationTheme: const InputDecorationTheme(
@@ -20,10 +33,10 @@ class AppThemes {
         ),
 
     //BUTTONS
-    elevatedButtonTheme: const ElevatedButtonThemeData(
-        style: ButtonStyle(
-            //TODO fill button themes
-            )),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(),
+    ),
+    //TODO fill button themes
     outlinedButtonTheme: const OutlinedButtonThemeData(
         style: ButtonStyle(
             //TODO fill button themes
--- lib/view/quiz_view.dart
+++ lib/view/quiz_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:queasy/model/answer.dart';
+
 class QuizView extends StatefulWidget {
   const QuizView({Key? key}) : super(key: key);
 
@@ -8,8 +10,58 @@ class QuizView extends StatefulWidget {
 }
 
 class _QuizViewState extends State<QuizView> {
+  String text = "This is the text of the first question.";
+  Answer answer1 = Answer("Answer 1", true);
+  Answer answer2 = Answer("Answer 2", true);
+  Answer answer3 = Answer("Answer 3", true);
+  Answer answer4 = Answer("Answer 4", true);
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Quiz View'));
+    double _width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: _width,
+              child: Text(
+                text,
+                style: Theme.of(context).textTheme.headline3,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(answer1.text),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(answer2.text),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(answer3.text),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(answer4.text),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
