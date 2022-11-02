971d30571616c99cce123b54a349c940ded29766
parkoriann <debiuti2@gmail.com>
2022-11-03 00:29:06 +0100

    themes and dependencies

--- lib/constants/app_themes.dart
+++ lib/constants/app_themes.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color orange = const Color(0xfff19c79);
+Color green = const Color(0xff9fc490);
 
 class AppThemes {
   ThemeData themeData = ThemeData(
     //COLORS
-    primarySwatch: Colors.deepPurple,
-    colorScheme: const ColorScheme.light(
-        //TODO fill colors
-        ),
+    colorScheme: ColorScheme.light(
+      primary: Color(0xff72479d),
+      secondary: orange,
+      background: Color(0xfff1ffe7),
+      onPrimary: Color(0xfff1ffe7),
+      onSecondary: Color(0xff0d0106),
+      onBackground: Color(0xff0d0106),
+      primaryContainer: Color(0xfff19c79),
+      secondaryContainer: green,
+      //TODO fill necessary colors
+    ),
 
     //TEXT
-    // fontFamily: ,
+    fontFamily: GoogleFonts.nunito().fontFamily,
     textTheme: const TextTheme(
-        //TODO fill text themes
-        ),
+      headline3: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+      ),
+
+      //TODO fill text themes
+    ),
 
     //TEXT FIELD
     inputDecorationTheme: const InputDecorationTheme(
@@ -20,10 +36,12 @@ class AppThemes {
         ),
 
     //BUTTONS
-    elevatedButtonTheme: const ElevatedButtonThemeData(
-        style: ButtonStyle(
-            //TODO fill button themes
-            )),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        textStyle: MaterialStateProperty.all(TextStyle(fontSize: 40)),
+      ),
+    ),
+    //TODO fill button themes
     outlinedButtonTheme: const OutlinedButtonThemeData(
         style: ButtonStyle(
             //TODO fill button themes
