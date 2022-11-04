e15cd55a74b1443c0a0496d97fb1f32b45aa5a7d
Sophia Soares <sophia.soares.silva@gmail.com>
2022-11-04 19:57:27 +0100

    Bottom nav bar added to profile, leaderboard and home views

--- lib/constants/app_themes.dart
+++ lib/constants/app_themes.dart
@@ -3,28 +3,37 @@ import 'package:google_fonts/google_fonts.dart';
 
 Color orange = const Color(0xfff19c79);
 Color green = const Color(0xff9fc490);
+Color purple = const Color(0xff72479d);
+Color white = const Color(0xfff1ffe7);
+Color black = const Color(0xff0d0106);
 
 class AppThemes {
   ThemeData themeData = ThemeData(
-    //COLORS
+    //COLOR
     colorScheme: ColorScheme.light(
-      primary: Color(0xff72479d),
+      primary: purple,
+      onPrimary: white,
       secondary: orange,
-      background: Color(0xfff1ffe7),
-      onPrimary: Color(0xfff1ffe7),
-      onSecondary: Color(0xff0d0106),
-      onBackground: Color(0xff0d0106),
-      primaryContainer: Color(0xfff19c79),
-      secondaryContainer: green,
+      onSecondary: black,
+      tertiary: green,
+      onTertiary: black,
+      background: white,
+      onBackground: black,
+
       //TODO fill necessary colors
     ),
 
     //TEXT
     fontFamily: GoogleFonts.nunito().fontFamily,
-    textTheme: const TextTheme(
-      headline3: TextStyle(
+    textTheme: TextTheme(
+      headline3: const TextStyle(
         fontSize: 30,
         fontWeight: FontWeight.bold,
+      ),
+      caption: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.bold,
+        color: purple,
       ),
 
       //TODO fill text themes
--- lib/view/leaderboard_view.dart
+++ lib/view/leaderboard_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'custom_bottom_nav_bar.dart';
 
 class LeaderboardView extends StatelessWidget {
   const LeaderboardView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Leaderboard View'));
+    return const Scaffold(
+      bottomNavigationBar: CustomBottomNavBar(pageTitle: 'Leaderboard'),
+      body: Center(
+        child: Text('Leaderboard View'),
+      ),
+    );
   }
 }
