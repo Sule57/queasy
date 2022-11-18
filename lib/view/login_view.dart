5353bb31c9c563bc2c30c42f1c15490e559654a9
nkreshpaj <nikolkreshpaj@gmail.com>
2022-11-18 16:23:24 +0100

    Login and category view

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:queasy/view/widgets/rounded-button.dart';
 
 import '../auth.dart';
 
@@ -35,112 +36,119 @@ class _LogInViewState extends State<LogInView> {
       resizeToAvoidBottomInset: false,
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:[
-            Padding(
-              padding: const EdgeInsets.only(top: 1.0, bottom: 7.0),
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height / 5,
-                child: Image.asset(
-                  'lib/assets/images/logo_and_name.png',
-                  fit: BoxFit.cover,
+        child: Container(
+          height: MediaQuery.of(context).size.height / 3,
+          color: Theme.of(context).colorScheme.onPrimary,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 1.0, bottom: 7.0),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height / 5,
+                  child: Image.asset(
+                    'lib/assets/images/logo_and_name.png',
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0, right:8.0),
-                  child:Container(
-                    padding: const EdgeInsets.only(left: 15.0, right:15.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius:
-                      const BorderRadius.all(Radius.circular(16.0)),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                    child: Container(
+                      padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(16.0)),
+                      ),
+                      child: Column(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(top: 10.0),
+                            child: Text(
+                              'Log In',
+                              style: TextStyle(
+                                  fontSize: 30.0, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Text(errorMessage == '' ? '' : 'Humm? $errorMessage'),
+                          TextField(
+                            controller: _controllerEmail,
+                            decoration: InputDecoration(
+                                labelText: 'Email',
+                                hintText: 'yourname@example.com'),
+                          ),
+                          TextField(
+                            controller: _controllerPassword,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelText: 'Enter Password',
+                              hintText: 'Password',
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10, bottom: 10),
+                            child: RoundedButton(
+                              buttonName: 'Log In',
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.primary,
+                              textColor:
+                                  Theme.of(context).colorScheme.background,
+                              /*onPressed: signInWithEmailAndPassword,*/
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    child: Column(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.only(top: 10.0),
-                          child: Text(
-                            'Log In',
-                            style: TextStyle(
-                                fontSize: 30.0, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Text(errorMessage == '' ? '' : 'Humm? $errorMessage'),
-                        TextField(
-                          controller: _controllerEmail,
-                          decoration: InputDecoration(
-                              labelText: 'Email', hintText: 'yourname@example.com'),
-                        ),
-                        TextField(
-                          controller: _controllerPassword,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: 'Enter Password',
-                            hintText: 'Password',
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top:10, bottom: 10),
-                          child: RoundedButton(
-                            buttonName: 'Log In',
-                            backgroundColor:Theme.of(context).colorScheme.primary,
-                            textColor: Theme.of(context).colorScheme.background,
-                            /*onPressed: signInWithEmailAndPassword,*/
-                          ),
-                        ),
-                      ],
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 10.0, right: 14.0),
+                      child: const Divider(
+                          color: Colors.grey, height: 36, thickness: 1.0),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.only(left: 10.0, right: 14.0),
-                    child: const Divider(
-                        color: Colors.grey, height: 36, thickness: 1.0),
+                  const Text("or sign up with",
+                      style: TextStyle(fontSize: 16.0)),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 14.0, right: 10.0),
+                      child: const Divider(
+                          color: Colors.grey, height: 36, thickness: 1.0),
+                    ),
                   ),
-                ),
-                const Text("or sign up with", style: TextStyle(fontSize: 16.0)),
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.only(left: 14.0, right: 10.0),
-                    child: const Divider(
-                        color: Colors.grey, height: 36, thickness: 1.0),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    icon: Image.asset('lib/assets/images/google.png'),
+                    onPressed: () {},
                   ),
-                ),
-              ],
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  icon: Image.asset('lib/assets/images/google.png'),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Image.asset(
-                    'lib/assets/images/facebook.png',
+                  IconButton(
+                    icon: Image.asset(
+                      'lib/assets/images/facebook.png',
+                    ),
+                    onPressed: () {},
                   ),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Image.asset(
-                    'lib/assets/images/twitter.png',
+                  IconButton(
+                    icon: Image.asset(
+                      'lib/assets/images/twitter.png',
+                    ),
+                    onPressed: () {},
                   ),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
