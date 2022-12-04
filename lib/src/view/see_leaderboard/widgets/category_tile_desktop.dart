6b65a1b869e827f8a7afa351b5e6c59bcf4af28a
Endia Clark <78235607+rhit-clarken@users.noreply.github.com>
2022-12-04 23:05:00 +0100

    desktop view changes based on category buttn press

--- lib/src/view/see_leaderboard/widgets/category_tile_desktop.dart
+++ lib/src/view/see_leaderboard/widgets/category_tile_desktop.dart
@@ -1,35 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:queasy/constants/app_themes.dart';
+import 'package:queasy/src/view/see_leaderboard/leaderboard_provider.dart';
 
 class CategoryTileDesktop extends StatelessWidget {
   final String title;
+  final List<String> entries;
 
-  const CategoryTileDesktop({super.key, required this.title});
+  const CategoryTileDesktop(
+      {super.key, required this.title, required this.entries});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        const Divider(
-          height: 20,
-        ),
+        // const Divider(
+        //   height: 20,
+        // ),
         Container(
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(15),
-            color: Theme.of(context).colorScheme.tertiary,
+            color: Color(0xFFF1FFE7),
           ),
           child: ListTile(
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-            tileColor: Theme.of(context).colorScheme.tertiary,
             textColor: Colors.black,
-            iconColor: Colors.white,
             title: Text(
               title,
               style: TextStyle(color: Colors.black, fontSize: 26),
             ),
-            //const Icon(Icons.back_hand_outlined),
-            //subtitle: index == 0 ? const Text('username', textAlign: TextAlign.center,style:TextStyle(color: Colors.black, fontSize: 26),): null,
+            onTap: () {
+              Provider.of<LeaderboardProvider>(context, listen: false)
+                  .setLeaderboard(title);
+            },
           ),
         ),
       ],
--- lib/src/view/see_leaderboard/widgets/user_tile_desktop.dart
+++ lib/src/view/see_leaderboard/widgets/user_tile_desktop.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:queasy/src/model/leaderboard_entry.dart';
 
 class UserTileDesktop extends StatelessWidget {
   final int index;
+  final List<LeaderboardEntry> entries;
 
-  const UserTileDesktop({super.key, required this.index});
+  const UserTileDesktop(
+      {super.key, required this.index, required this.entries});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        const Divider(
-          height: 20,
-        ),
+        // const Divider(
+        //   height: 20,
+        // ),
         Container(
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(15),
             color: Colors.white,
           ),
           child: ListTile(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            // shape:
+            //     RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
             tileColor: Colors.white,
             textColor: const Color(0xFFFF8C66),
             iconColor: Colors.white,
@@ -36,12 +39,12 @@ class UserTileDesktop extends StatelessWidget {
                     color: Colors.white,
                   ),
                 ))),
-            title: const Text(
-              'username',
+            title: Text(
+              entries[index].getName,
               style: TextStyle(color: Colors.black, fontSize: 26),
             ),
-            subtitle: const Text(
-              'points',
+            subtitle: Text(
+              entries[index].getScore.toString(),
             ),
             //const Icon(Icons.back_hand_outlined),
             //subtitle: index == 0 ? const Text('username', textAlign: TextAlign.center,style:TextStyle(color: Colors.black, fontSize: 26),): null,
