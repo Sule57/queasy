18c1b4671d3d71dbbd633990db4ced4dc1e07660
parkoriann <debiuti2@gmail.com>
2023-01-07 11:29:38 +0100

    add methods in auth: - sign out - sign in with google - sign in with facebook - //sign in with twitter - send reset password email (//TODO)

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,12 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 
-class Auth {
+class Auth with ChangeNotifier {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   User? get currentUser => _firebaseAuth.currentUser;
 
   Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
-
 
   Future<void> signInWithEmailAndPassword({
     required String email,
@@ -16,5 +16,53 @@ class Auth {
       email: email,
       password: password,
     );
+    notifyListeners();
+  }
+
+  Future<bool> signOut() async {
+    try {
+      FirebaseAuth.instance.signOut();
+      notifyListeners();
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<void> signInWithGoogle() async {
+    final GoogleAuthProvider googleProvider = GoogleAuthProvider();
+
+    if (kIsWeb) {
+      await _firebaseAuth.signInWithPopup(googleProvider);
+    } else {
+      //TODO implement mobile sign in
+    }
+    //TODO register user in firestore with email and give him a choice for username
+  }
+
+  Future<void> signInWithFacebook() async {
+    final FacebookAuthProvider facebookProvider = FacebookAuthProvider();
+
+    if (kIsWeb) {
+      await _firebaseAuth.signInWithPopup(facebookProvider);
+    } else {
+      //TODO implement mobile sign in
+    }
+    //TODO register user in firestore with email and give him a choice for username
+  }
+
+  Future<void> signInWithTwitter() async {
+    final TwitterAuthProvider twitterProvider = TwitterAuthProvider();
+
+    if (kIsWeb) {
+      await _firebaseAuth.signInWithPopup(twitterProvider);
+    } else {
+      //TODO implement mobile sign in
+    }
+    //TODO register user in firestore with email and give him a choice for username
+  }
+
+  void sendPasswordResetEmail(String emailAddress) {
+    //TODO implement
   }
 }
