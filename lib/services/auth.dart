8c381ed5cce256128ce452d649882290840468e7
parkoriann <debiuti2@gmail.com>
2023-01-07 11:59:49 +0100

    merge google-register
    Signed-off-by: parkoriann <debiuti2@gmail.com>

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -37,24 +37,21 @@ class Auth with ChangeNotifier {
       await _firebaseAuth.signInWithPopup(googleProvider);
     } else {
       //TODO implement mobile sign in
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+      // return await FirebaseAuth.instance.signInWithCredential(credential);
     }
     //TODO register user in firestore with email and give him a choice for username
-  }
-
-  Future<UserCredential> signInWithGoogle() async {
-    // Trigger the authentication flow
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-    // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth = await googleUser
-        ?.authentication;
-
-    // Create a new credential
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
-    );
-    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
   Future<void> signInWithFacebook() async {
