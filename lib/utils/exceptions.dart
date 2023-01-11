26a5ba4a5be58af266ec8813ceb6c63f9f5b300f
Savo Simeunovic <62600711+Rek27@users.noreply.github.com>
2023-01-11 16:37:04 +0100

    randomizer and comments

--- lib/utils/exceptions.dart
+++ lib/utils/exceptions.dart
@@ -1,5 +1,5 @@
 class UserDoesNotExistException implements Exception {
-  String errMsg() => 'This user does not exist';
+  String errMsg() => 'This user does not exist ';
 
 }
 
@@ -14,4 +14,16 @@ class UserAlreadyExistsException implements Exception{
 
 class CategoryNotFoundException implements Exception{
   String errMsg() => 'Category not found ';
+}
+
+class JSONIsNotFormattedCorrectlyException implements Exception{
+  String errMsg() => 'Invalid JSON format for Category ';
+}
+
+class QuestionNotFoundException implements Exception{
+  String errMsg() => 'Question is not found in the database';
+}
+
+class CategoryAlreadyExistsException implements Exception{
+  String errMsg() => 'Category already exists in the database ';
 }
