b17b738548ec301170e42cdb74fecfa5fc0a384d
parkoriann <debiuti2@gmail.com>
2023-01-13 00:07:39 +0100

    add exports
    delete unused files
    
    Signed-off-by: parkoriann <debiuti2@gmail.com>

---
+++ lib/exports/view_login.dart
@@ -0,0 +1,5 @@
+export '../src/view/login/login_provider.dart';
+export '../src/view/login/login_view.dart';
+export '../src/view/login/widgets/forgot_password_dialog.dart';
+export '../src/view/login/widgets/login_info.dart';
+export '../src/view/login/widgets/social_media_buttons.dart';
---
+++ lib/exports/view_see_questions.dart
@@ -0,0 +1,5 @@
+export '../src/view/see_questions/category_questions_view.dart';
+export '../src/view/see_questions/category_questions_provider.dart';
+export '../src/view/see_questions/widgets/question_list_tile.dart';
+export '../src/view/see_questions/widgets/questions_popups.dart';
+export '../src/view/see_questions/widgets/questions_text_field.dart';
